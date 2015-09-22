d230dfedd8080d49c2c534932476c9aa306de6f6
Gustavo Gutierrez <gustavogut1993@gmail.com>
2015-09-22 14:41:34 -0430

    Cambiando el Anchor de las regex: - ^ Indica comienzo de línea, se reemplazo por \A para comienzo de String

--- lexer.rb
+++ lexer.rb
@@ -4,19 +4,17 @@ def lexer(file)
     column = 1
     has_errors = false
     while not program.empty?
-        # puts "Programa:\n'#{program}'\n"
-        
-        if (/^ +/ =~ program)
-            sub = program.slice!(/^ +/)
+        # puts "Programa:\n'#{program}'\n\n"
+        case program
+        when /\A +/s 
+            sub = program.slice!(/\A +/)
             column += sub.length
-        
-        elsif (/^\n/ =~ program)
-            program.slice!(/^\n/)
-            puts "Linea #{line} tiene #{column} columnas."
+        when /\A\n/
+            program.slice!(/\A\n/)
             line += 1
             column = 1
         else
-            sub = program.slice!(/^.+/)
+            sub = program.slice!(/\A.+/)
             column += sub.length
         end
 
