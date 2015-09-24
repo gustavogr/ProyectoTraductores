2a9555b83185db7fceeaca915263e455edc757a0
Gustavo Gutierrez <gustavogut1993@gmail.com>
2015-09-24 13:27:46 -0430

    Big work papah, casi listo, falta probar y arreglar entrega: - Se agrego el token /= - Se hicieron todas las reglas - Se coloco un pequeño comentario para que aplica cada regla

--- lexer.rb
+++ lexer.rb
@@ -5,50 +5,99 @@ def lexer(file)
     reservedW = /\A(bot|execute|if|create|else|while|int|bool|char|store|recieve|on|end|activate|activation|advance|
              deactivate|deactivation|default|collect|as|drop|left|right|up|down|read|true|false)/
 
-    symbols   = /\A(>=|<=|\\\/|\/\\|,|\.|:|\+|-|\(|\)|\*|\/|%|~|<|>|=)/
+    symbols   = /\A(>=|<=|\\\/|\/\\|\/=|,|\.|:|\+|-|\(|\)|\*|\/|%|~|<|>|=)/
 
-    tokenHash = Hash[","   =>  "Coma", "."   =>  "Punto", ":"   =>  "DosPuntos", "("   =>  "ParAbre",
-                 ")"   =>  "ParCierra", "+"   => "Suma", "-"   => "Resta", "*"   => "Mult", "/"   => "Div", 
-                "%"   => "Mod", "\/\\"  => "Conjuncion", "\\\/"  => "Disyuncion", "~"   => "Negacion", 
-                "<"   => "Menor", "<="  => "MenorIgual", ">"   => "Mayor", ">="  => "MayorIgual", "="   => "Igual"]
-
+    tokenHash = Hash[   ","   =>  "Coma",
+                        "."   =>  "Punto",
+                        ":"   =>  "DosPuntos",
+                        "("   =>  "ParAbre",
+                        ")"   =>  "ParCierra",
+                        "+"   => "Suma",
+                        "-"   => "Resta",
+                        "*"   => "Mult",
+                        "/"   => "Div",
+                        "%"   => "Mod",
+                        "/\\" => "Conjuncion",
+                        "\\/" => "Disyuncion",
+                        "~"   => "Negacion",
+                        "<"   => "Menor",
+                        "<="  => "MenorIgual",
+                        ">"   => "Mayor",
+                        ">="  => "MayorIgual",
+                        "="   => "Igual",
+                        "/="  => "NoIgual"
+                    ]
 
     program = File.read(file)
     line = 1
     column = 1
-    has_errors = false
+    hasErrors = false
+    tokensList = []
     while not program.empty?
         case program
+        ## Espacios vacios
         when /\A +/s 
             sub = program.slice!(/\A +/)
             column += sub.length
 
-        when /#{reservedW}\W/ 
+        ## Palabras Reservadas
+        when /#{reservedW}(\W|\z)/ 
             sub = program.slice!(reservedW)
-            print Token.new(sub.capitalize, line, column)
+            tokensList << Token.new(sub.capitalize, line, column)
             column += sub.length
-            
+
+        ## Saltos de línea
         when /\A\n/
             program.slice!(/\A\n/)
             line += 1
             column = 1
 
+        ## Símbolos
         when symbols
-            print Token.new(tokenHash[program[0]], line, column)
+            sub = program.slice!(symbols)
+            tokensList << Token.new(tokenHash[sub], line, column)
+            column += sub.length
+
+        ## Caracteres
+        when /\A\w(\W|\z)/
+            sub = program.slice!(/\A\w/)
+            tokensList << TkChar.new(sub, line, column)
+            column += sub.length
+
+        ## Numeros
+        when /\A\d+(\W|\z)/
+            sub = program.slice!(/\A\d+/)
+            tokensList << TkNum.new(sub, line, column) 
+            column += sub.length
+
+        ## Identificadores
+
+        when /\A\w+(\W|\z)/
+            sub = program.slice!(/\A[a-zA-Z]\w+/)
+            tokensList << TkIdent.new(sub, line, column)
+            column += sub.length
+
+        ## Comentarios de una línea
+        when /\A\$\$.*$/
+            sub = program.slice!(/\A\$\$.*$/)
+
+        when /\A\$-.*?-\$/m
+            sub = program.slice!(/\A\$-.*?-\$/m)
+            line += sub.count("\n")
+            lastLine = sub.slice!(/^.*\z/)
+            column = lastLine.length + 1
+
+        else
+            sub = program.slice!(0)
+            hasErrors = true
+            puts "Error: Caracter inesperado \"#{sub}\" en la fila #{line}, columna #{column}"
             column += 1
-            program[0] = ''
-
-        #Hasta ahora todo esto lo tomo como identificadores
-        #hay que seguir refinando, para sacar Nums y chars.
-        when /\A\w+ /
-            sub = program.slice!(/\A\w+ /)
-            #print TIdent.new("TkIdent", sub,  line, column)
-            column += sub.length - 1
-        #     has_errors = true
-        end
-
     
+        end 
     end
+
+    puts tokensList
+    return tokensList
 end
 
 
