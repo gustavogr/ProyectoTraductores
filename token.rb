4955e5327bd533cce5213d88b07706913aeb1ad8
Gustavo Gutierrez <gustavogut1993@gmail.com>
2015-09-23 21:14:53 -0430

    Refactorizadas las clases de los tokens: - Se simplifico el esquema de herencia - Las clases especificas sobreescriben el initialize

--- token.rb
+++ token.rb
@@ -5,33 +5,46 @@ class Token
         @column = column
     end
     def to_s
-        "#{@name} #{@line} #{@column}"
+        "#Tk{@name} #{@line} #{@column}"
     end
 end
 
-class WArgs < Token 
-    def initialize(name, argument, line, column)
-        super(name, line, column)
+class TkIdent < Token
+    def initialize(argument, line, column)
         @argument = argument
+        @name = "Ident"
+        @line = line
+        @column = column
     end
 
-end
-
-class TIdent < WArgs
     def to_s
-        "#{@name}(\"#{@argument}\") #{@line} #{@column}"
-    end
-end
-
-class TChar < WArgs
-    def to_s
-        "#{@name}('#{@argument}') #{@line} #{@column}"
+        "Tk#{@name}(\"#{@argument}\") #{@line} #{@column}"
     end
 end
 
-class TNum < WArgs
+class TkChar < Token
+    def initialize(argument, line, column)
+        @argument = argument
+        @name = "Character"
+        @line = line
+        @column = column
+    end
+
     def to_s
-        "#{@name}(#{@argument}) #{@line} #{@column}"
+        "Tk#{@name}('#{@argument}') #{@line} #{@column}"
+    end
+end
+
+class TkNum < Token
+    def initialize(argument, line, column)
+        @argument = argument
+        @name = "Num"
+        @line = line
+        @column = column
+    end
+
+    def to_s
+        "Tk#{@name}(#{@argument}) #{@line} #{@column}"
     end
 end
 
