f0ca273e042d93a9c8f36395188aac99a5e0e3f7
Jose Pascarella <josepas979@gmail.com>
2016-02-07 17:21:15 -0430

    Lexer ahora es Clase, le agregue next_token para racc

--- lexer.rb
+++ lexer.rb
@@ -15,7 +15,7 @@
 
 require_relative 'token.rb'
 
-def lexer(file)
+class Lexer
 
     # Regexp que hace match con cualquier palabra reservada
     
@@ -53,114 +53,119 @@ def lexer(file)
                         "="   => "Igual",
                         "/="  => "NoIgual"
                     ]
-
-    program = File.read(file)   # Programa a Analizar
-    line = 1                    # Contador para mostrar el numero de linea
-    column = 1                  # Contador para mostrar el numero de columna
-    hasErrors = false           # Booleano para recordar si hubo un error
-    tokensList = []             # La lista de tokens a retornar
-    
+    attr_accessor :tokensList 
+        
     ##
     #  Iteramos sobre el programa a analizar.
     #  En cada iteracion se hace match del inicio
     #  del programa con las diferentes Regexp y se procesa
     #  según sea el caso.
+    def analize(file)
+        program = File.read(file)   # Programa a Analizar
+        line = 1                    # Contador para mostrar el numero de linea
+        column = 1                  # Contador para mostrar el numero de columna
+        hasErrors = false           # Booleano para recordar si hubo un error
 
-    while not program.empty?
+        while not program.empty?
+            
+
+            case program
+            
+            ## Espacios vacios
+            #  Se extraen todos los espacios posibles y se aumenta la columna
+            when /\A +/s 
+                sub = program.slice!(/\A +/)
+                column += sub.length
+
+            ## Palabras Reservadas
+            #  Se usa la regexp reservedW. Luego de la palabra no puede venir 
+            #  un caracter valido para palabra debido a que dejaria de ser una
+            #  palabra reservada.
+            when /#{reservedW}(\W|\z)/ 
+                sub = program.slice!(reservedW)
+                @tokensList << Token.new(sub.upcase.to_sym, sub.capitalize, line, column)
+                column += sub.length
+
+            ## Saltos de línea
+            #  Se aumenta el numero de linea y se reinicia el numero de columna a 1
+            when /\A\n/
+                program.slice!(/\A\n/)
+                line += 1
+                column = 1
+
+            ## Símbolos
+            #  Se usa la regexp symbols. A diferencia del caso de las palabras
+            #  reservadas no nos afecta que venga despues del simbolo
+            when symbols
+                sub = program.slice!(symbols)
+                @tokensList << Token.new(tokenHash[sub].upcase.to_sym, sub, line, column)
+                column += sub.length
+
+            ## Caracteres
+            #  Hace match con un solo caracter. Al igual que con las palabras
+            #  reservadas se pide que no venga un caracter valido de palabra
+            #  despues.
+            when /\A[a-zA-Z](\W|\z)/
+                sub = program.slice!(/\A\w/)
+                @tokensList << TkChar.new(sub, line, column)
+                column += sub.length
+
+            ## Numeros
+            #  Hace match con cualquier secuencia de numeros. 
+            #  Tambien se verifica que no siga un caracter valido de palabra
+            when /\A\d+(\W|\z)/
+                sub = program.slice!(/\A\d+/)
+                @tokensList << TkNum.new(sub, line, column) 
+                column += sub.length
+
+            ## Identificadores
+            #  Deben empezar con una letra y luego puede venir cualquier
+            #  combinacion de caracteres de palabra.
+            when /\A[a-zA-Z]\w+(\W|\z)/
+                sub = program.slice!(/\A[a-zA-Z]\w+/)
+                @tokensList << TkIdent.new(sub, line, column)
+                column += sub.length
+
+            ## Comentarios de una línea
+            #  Hace match desde la ocurrencia de $$ hasta el final de esa linea
+            when /\A\$\$.*$/
+                sub = program.slice!(/\A\$\$.*$/)
+
+            ## Comentarios multilinea
+            #  Se extraen todas las lineas que esten entre $- y -$. 
+            #  Una vez extraidas se ajustan los contadores de manera adecuada.
+            when /\A\$-.*?-\$/m
+                sub = program.slice!(/\A\$-.*?-\$/m)
+                line += sub.count("\n")
+                lastLine = sub.slice!(/^.*\z/)
+                column = lastLine.length + 1
+
+            ## Identificadores no validos
+            #  Extrae los identificadores que empiezan con un numero o un _
+            #  Este caso reporta el error.
+            when /\A[\d_]\w/
+                sub = program.slice!(/\A[\d_]+/)
+                hasErrors = true
+                puts "Error: Caracter inesperado \"#{sub[0]}\" en la fila #{line}, columna #{column}"
+                column += sub.length
+
+            ## Errores
+            #  Si no cae en ninguna de las reglas anteriores estamos ante un error
+            #  Se reporta el error y se consume un caracter para seguir avanzando
+            else
+                sub = program.slice!(0)
+                hasErrors = true
+                puts "Error: Caracter inesperado \"#{sub}\" en la fila #{line}, columna #{column}"
+                column += 1
         
+            end 
+        end
 
-        case program
-        
-        ## Espacios vacios
-        #  Se extraen todos los espacios posibles y se aumenta la columna
-        when /\A +/s 
-            sub = program.slice!(/\A +/)
-            column += sub.length
+        def next_token
+            token = @tokensList.shift
+            token == nil ? nil : token.get_token
+        end
 
-        ## Palabras Reservadas
-        #  Se usa la regexp reservedW. Luego de la palabra no puede venir 
-        #  un caracter valido para palabra debido a que dejaria de ser una
-        #  palabra reservada.
-        when /#{reservedW}(\W|\z)/ 
-            sub = program.slice!(reservedW)
-            tokensList << Token.new(sub.upcase.to_sym, sub.capitalize, line, column)
-            column += sub.length
-
-        ## Saltos de línea
-        #  Se aumenta el numero de linea y se reinicia el numero de columna a 1
-        when /\A\n/
-            program.slice!(/\A\n/)
-            line += 1
-            column = 1
-
-        ## Símbolos
-        #  Se usa la regexp symbols. A diferencia del caso de las palabras
-        #  reservadas no nos afecta que venga despues del simbolo
-        when symbols
-            sub = program.slice!(symbols)
-            tokensList << Token.new(tokenHash[sub].upcase.to_sym, sub, line, column)
-            column += sub.length
-
-        ## Caracteres
-        #  Hace match con un solo caracter. Al igual que con las palabras
-        #  reservadas se pide que no venga un caracter valido de palabra
-        #  despues.
-        when /\A[a-zA-Z](\W|\z)/
-            sub = program.slice!(/\A\w/)
-            tokensList << TkChar.new(sub, line, column)
-            column += sub.length
-
-        ## Numeros
-        #  Hace match con cualquier secuencia de numeros. 
-        #  Tambien se verifica que no siga un caracter valido de palabra
-        when /\A\d+(\W|\z)/
-            sub = program.slice!(/\A\d+/)
-            tokensList << TkNum.new(sub, line, column) 
-            column += sub.length
-
-        ## Identificadores
-        #  Deben empezar con una letra y luego puede venir cualquier
-        #  combinacion de caracteres de palabra.
-        when /\A[a-zA-Z]\w+(\W|\z)/
-            sub = program.slice!(/\A[a-zA-Z]\w+/)
-            tokensList << TkIdent.new(sub, line, column)
-            column += sub.length
-
-        ## Comentarios de una línea
-        #  Hace match desde la ocurrencia de $$ hasta el final de esa linea
-        when /\A\$\$.*$/
-            sub = program.slice!(/\A\$\$.*$/)
-
-        ## Comentarios multilinea
-        #  Se extraen todas las lineas que esten entre $- y -$. 
-        #  Una vez extraidas se ajustan los contadores de manera adecuada.
-        when /\A\$-.*?-\$/m
-            sub = program.slice!(/\A\$-.*?-\$/m)
-            line += sub.count("\n")
-            lastLine = sub.slice!(/^.*\z/)
-            column = lastLine.length + 1
-
-        ## Identificadores no validos
-        #  Extrae los identificadores que empiezan con un numero o un _
-        #  Este caso reporta el error.
-        when /\A[\d_]\w/
-            sub = program.slice!(/\A[\d_]+/)
-            hasErrors = true
-            puts "Error: Caracter inesperado \"#{sub[0]}\" en la fila #{line}, columna #{column}"
-            column += sub.length
-
-        ## Errores
-        #  Si no cae en ninguna de las reglas anteriores estamos ante un error
-        #  Se reporta el error y se consume un caracter para seguir avanzando
-        else
-            sub = program.slice!(0)
-            hasErrors = true
-            puts "Error: Caracter inesperado \"#{sub}\" en la fila #{line}, columna #{column}"
-            column += 1
-    
-        end 
-    end
-
-    return tokensList unless hasErrors
+    end    
 end
 
